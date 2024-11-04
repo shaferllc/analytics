b319608552cbe1c03592d41bfbf201d530d2533a
Tom Shafer <tj@tjshafer.com>
2024-11-04 13:13:51 -0700

    wip

---
+++ database/factories/StatFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Shaferllc\Analytics\Models\Stat;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StatFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Stat::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'views' => $this->faker->numberBetween(0, 1000000),
+            'likes' => $this->faker->numberBetween(0, 10000),
+            'shares' => $this->faker->numberBetween(0, 5000),
+            'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'updated_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
+        ];
+    }
+}
